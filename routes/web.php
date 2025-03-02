752ecae6042848b0af6acb70f328bad20c3147c2
odeandialamsyah <odeandialamsyah@gmail.com>
2025-03-03 03:26:21 +0700

    taskController && model task

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,25 @@
 <?php
 
-
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\AuthController;
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\registerController;
-
 // Routes untuk auth
-
-
 Route::get('/', [AuthController::class, 'showLoginForm']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-
-//registration
-Route::get('/registration', [registerController::class, 'showRegistrationForm'])->middleware('guest');
-Route::post('/registration', [registerController::class, 'register'])->name('create.user');
-
-
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    // Other authenticated routes...
+    
+    //Task
+    Route::get('/tasks', [TaskController::class, 'getAllTask']);
+    Route::get('/task/{id}', [TaskController::class, 'getTaskById']);
+    Route::post('/task', [TaskController::class, 'insertTask']);
+    Route::put('/task', [TaskController::class, 'updateTask']);
+    Route::delete('/task', [TaskController::class, 'deleteTask']);
 });
 
 
