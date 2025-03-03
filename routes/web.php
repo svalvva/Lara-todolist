6d782570868c2751f041b0ac73628c7bf3313803
rendykml <rd24rk@gmail>
2025-03-03 19:16:18 +0700

    rubah route

--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,16 @@ Route::get('/', [AuthController::class, 'showLoginForm']);
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->name('create.user');
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    
-    //Task
-    Route::get('/tasks', [TaskController::class, 'getAllTask']);
-    Route::get('/task/{id}', [TaskController::class, 'getTaskById']);
-    Route::post('/task', [TaskController::class, 'insertTask']);
-    Route::put('/task', [TaskController::class, 'updateTask']);
-    Route::delete('/task', [TaskController::class, 'deleteTask']);
+
+    // Task Routes
+    Route::get('/tasks', [TaskController::class, 'getAllTask'])->name('tasks.index');
+    Route::get('/task/{id}', [TaskController::class, 'getTaskById'])->name('tasks.show');
+    Route::post('/task', [TaskController::class, 'insertTask'])->name('tasks.store');
+    Route::put('/tasks/{id}', [TaskController::class, 'updateTask'])->name('tasks.update');
+    Route::delete('/tasks/{id}', [TaskController::class, 'deleteTask'])->name('tasks.destroy');
+
 });
-
-
-
